806d2ae1643937321e76a5a30e2db8524087bedb
fachrudahri <fachru.dahru@gmail.com>
2022-01-16 17:25:04 +0700

    API with dynamic route & suspense

---
+++ src/components/pages/Post.vue
@@ -0,0 +1,20 @@
+<template>
+  <Suspense>
+    <template #default>
+      <PostDetail />
+    </template>
+
+    <template #fallback>
+      <span>loading...</span>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import PostDetail from '../PostDetail.vue'
+export default {
+  components: { PostDetail },
+  setup() {
+  }
+}
+</script>
---
+++ src/components/pages/Posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <Suspense>
+    <template #default>
+      <PostList />
+    </template>
+
+    <template #fallback>
+      <span>loading...</span>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import PostList from '../PostList.vue'
+export default {
+  components: { PostList },
+  setup() {
+  }
+}
+</script>
