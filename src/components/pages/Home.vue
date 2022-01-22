a25d70cc54db235b75051a37731b8533af6d9398
fachrudahri <fachru.dahru@gmail.com>
2022-01-22 11:39:46 +0700

    added map helpers

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -3,31 +3,59 @@
   <div>
     Homepage
   </div>
+  <hr>
+  <button @click="increment">add Like</button>
+  <button @click="setUsername('Fachru ganteng banget')">change username</button>
+  <button @click="getCredential">change credential</button>
+  <hr>
   <p>total Like: {{ likes }}</p>
+  <p>fake total like: {{ fakeTotalLikes }}</p>
+  <!-- <p>fake total like: {{ fakeLikes }}</p> -->
   <pre>{{ credential }}</pre>
 </template>
 
 <script>
-import { computed, ref, onMounted, compile } from "vue"
-import { useStore } from "vuex"
+import { computed, ref, onMounted } from "vue"
+import { useStore, mapState, mapGetters, mapMutations, mapActions } from "vuex"
 export default {
-  setup() {
-    const store = useStore()
-    const username = ref('')
+  //composition API with useStore vuex
+  // setup() {
+  //   const store = useStore()
+  //   const username = ref('')
 
-    const likes = computed(() => {
-      return store.state.totalLikes
-    })
+  //   const likes = computed(() => {
+  //     return store.state.totalLikes
+  //   })
 
-    const credential = computed(() => {
-      return store.state.credential
-    })
+  //   const credential = computed(() => {
+  //     return store.state.credential
+  //   })
 
-    onMounted(() => {
-      username.value = store.state.username
-    })
+  //   const fakeLikes = computed(() => {
+  //     return store.getters.fakeTotalLikes
+  //   })
 
-    return { likes, username, credential }
+  //   onMounted(() => {
+  //     username.value = store.state.username
+  //   })
+
+  //   return { likes, username, credential, fakeLikes }
+  // }
+
+  //option API with map vuex
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState(['username', 'credential']),
+    ...mapState({ likes: 'totalLikes' }),
+    ...mapGetters(['fakeTotalLikes'])
+  },
+  methods: {
+    ...mapMutations(['increment', 'setUsername']),
+    ...mapActions(['getCredential'])
   }
 }
 </script>
